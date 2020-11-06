771f3d5e1be6f51b4f7a2cd7b43749b561e3a97a
ritesh-modi <ritesh-modi@outlook.com>
2020-11-06 13:40:12 +0530

    added tags to resources in module1

--- modules/module1/rg.tf
+++ modules/module1/rg.tf
@@ -19,19 +19,13 @@ resource azurerm_resource_group myrg {
     name= var.rgname1
     location  = var.location1
 
-    tags {
-        ritesh = "modi"
-        aa = "bb"
-    }
+
 }
 
 resource azurerm_resource_group myrg1 {
     name= var.rgname2
     location  = var.location2
 
-    tags  {
-        ritesh = "modi"
-    }
 }
 
 output myoutput1 {
